e74a8dc51e8a129af22b6795c600ea91db734c46
Manpreet Singh <manpreesingh@qasource.com>
2021-10-15 12:04:35 +0530

    chore: turned skipfailure to true

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -6,27 +6,27 @@ describe("[Test Accessibility for {PAGE} and {URL}]", () => {
 
   it("Run audit on full page", () => {
     cy.runAudit({
-      skipFailure: false,
+      skipFailure: true,
     });
   });
 
   it("Run audit on navbar", () => {
     cy.runAudit({
       context: "nav.navbar",
-      skipFailure: false,
+      skipFailure: true,
     });
   });
 
   it("Run audit on header", () => {
     cy.runAudit({
       context: "div.banner",
-      skipFailure: false,
+      skipFailure: true,
     });
   });
 
   it("Run audit on header but skip rule contrast", () => {
     cy.runAudit({
-      skipFailure: false,
+      skipFailure: true,
       context: "div.banner",
       options: {
         rules: {
@@ -38,6 +38,7 @@ describe("[Test Accessibility for {PAGE} and {URL}]", () => {
 
   it("Run audit on allLinks", () => {
     cy.runAudit({
+      skipFailure: true,
       context: "a",
     });
   });
